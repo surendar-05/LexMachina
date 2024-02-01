d85bab5eed758fe13ad95f724a8141f28761dfbf
KarthiDreamr <karthidreamr@gmail.com>
2024-02-01 13:33:45 +0530

    added google fonts 'Inter'

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:gene/authentication/sign_in.dart';
+import 'package:gene/authentication/sign_in_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
@@ -21,6 +25,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: GoogleFonts.interTextTheme(
+            Theme.of(context).textTheme), // Apply the Inter font
+        // TODO: Apply the Inter font offline
       ),
       home: const SignIn(),
     );
