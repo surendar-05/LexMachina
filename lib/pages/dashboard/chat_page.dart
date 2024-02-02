97fd049fa3af5e4472926f9415ebb61dd3cef6a3
KarthiDreamr <karthidreamr@gmail.com>
2024-02-02 19:28:34 +0530

    added chatpage

---
+++ lib/pages/dashboard/chat_page.dart
@@ -0,0 +1,152 @@
+import '../../message.dart';
+import 'package:chat_bubbles/bubbles/bubble_normal.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:http/http.dart' as http;
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  TextEditingController controller = TextEditingController();
+  ScrollController scrollController = ScrollController();
+  List<Message> msgs = [];
+  bool isTyping = false;
+
+  void sendMsg() async {
+    String text = controller.text;
+    // String apiKey = "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";
+    controller.clear();
+    try {
+      if (text.isNotEmpty) {
+        setState(() {
+          msgs.insert(0, Message(true, text));
+          isTyping = true;
+        });
+        scrollController.animateTo(0.0,
+            duration: const Duration(seconds: 1), curve: Curves.easeOut);
+        var response = await http.post(
+          Uri.parse(
+              "https://f321-203-193-157-130.ngrok-free.app/?question=$text"),
+        );
+        if (response.statusCode == 200) {
+          var textResponse = response.body;
+          setState(() {
+            isTyping = false;
+            msgs.insert(0, Message(false, textResponse));
+          });
+          scrollController.animateTo(0.0,
+              duration: const Duration(seconds: 1), curve: Curves.easeOut);
+        }
+      }
+    } on Exception catch (e) {
+      print('Exception occoured: $e');
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Some error occurred, please try again!")));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Chat Bot"),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 8,
+          ),
+          Expanded(
+            child: ListView.builder(
+                controller: scrollController,
+                itemCount: msgs.length,
+                shrinkWrap: true,
+                reverse: true,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: isTyping && index == 0
+                        ? Column(
+                            children: [
+                              BubbleNormal(
+                                text: msgs[0].msg,
+                                isSender: true,
+                                color: Colors.blue.shade100,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 16, top: 4),
+                                child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text("Typing...")),
+                              )
+                            ],
+                          )
+                        : Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: MarkdownBody(
+                              data: msgs[index].msg,
+                              shrinkWrap: true,
+                            ),
+                          ),
+                  );
+                }),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: TextField(
+                        controller: controller,
+                        textCapitalization: TextCapitalization.sentences,
+                        onSubmitted: (value) {
+                          sendMsg();
+                        },
+                        textInputAction: TextInputAction.send,
+                        showCursor: true,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none, hintText: "Enter text"),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  sendMsg();
+                },
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
