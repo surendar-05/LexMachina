c7d963d8b5e70ad04f284765fa052b3554168842
karthidreamr <karthidreamr@gmail.com>
2024-10-19 19:48:23 +0000

    updated packages

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:chatview/chatview.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
 import 'package:lexmachina/src/authentication/sign_in_screen.dart';
+import 'package:lexmachina/src/chat_ui/chat_screen.dart';
 
 import '/src/blog/blog_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
-import '/src/dashboard/dashboard.dart';
+// import '/src/dashboard/dashboard.dart';
 import '/src/authentication/sign_up_screen.dart';
 import 'src/onboarding/onboarding_screen.dart';
 import 'package:go_router/go_router.dart';
@@ -50,19 +50,6 @@ class MyApp extends StatelessWidget {
       otherUsers: [ChatUser(id: '2', name: 'Simform')],
     );
 
-    void onSendTap(
-        String message, ReplyMessage replyMessage, MessageType messageType) {
-      final message = Message(
-        id: '3',
-        message: "How are you",
-        createdAt: DateTime.now(),
-        sentBy: "user3",
-        replyMessage: replyMessage,
-        messageType: messageType,
-      );
-      chatController.addMessage(message);
-    }
-
     // Define your GoRouter here
     final GoRouter _router = GoRouter(
       routes: [
@@ -70,10 +57,10 @@ class MyApp extends StatelessWidget {
           path: '/',
           builder: (context, state) => const OnboardScreen(),
         ),
-        GoRoute(
-          path: '/dashboard',
-          builder: (context, state) => const Dashboard(),
-        ),
+        // GoRoute(
+        //   path: '/dashboard',
+        //   builder: (context, state) => const Dashboard(),
+        // ),
         GoRoute(
           path: '/signIn',
           builder: (context, state) => const SignIn(),
@@ -86,9 +73,10 @@ class MyApp extends StatelessWidget {
           path: '/blog',
           builder: (context, state) => BlogPage(),
         ),
-
-      
-
+        GoRoute(
+          path: '/chatScreen',
+          builder: (context, state) => ChatScreen(),
+        ),
       ],
     );
 
